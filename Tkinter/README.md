74699f9ed031507b78f48f9de28cf9e79b25760e
Koyo126 <168168540+Koyo126@users.noreply.github.com>
2025-06-12 12:43:32 +0900

    On branch develop
      -> Python 標準の GUI ツールキット `Tkinter` の使い方
    
    	new file:   Tkinter/README.md
    	new file:   Tkinter/Tutorials/.gitignore
    	new file:   Tkinter/Tutorials/test.py

---
+++ Tkinter/README.md
@@ -0,0 +1,5 @@
+# 概要
+Python 標準の GUI ツールキット `Tkinter` の使い方
+
+# 使用した python の version
+`Python 3.9.19`
---
+++ Tkinter/Tutorials/test.py
@@ -0,0 +1,23 @@
+### Python 3.9.19
+import sys
+import tkinter
+
+### 実行内容は
+###
+###   `root = tkinter.Tk()`
+###
+### と
+###
+###   `root.mainloop()`
+###
+### の間に記載する
+root = tkinter.Tk()
+
+### ウインドウのタイトルバーの表記
+root.title(u"Software Title")
+
+### ウインドウサイズ
+root.geometry("400x300")
+
+root.mainloop()
+
